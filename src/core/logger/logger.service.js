5909f12056997f08beb3a262074575db462df7c5
Borz Alexandru Mihai <alexandru.borz94@gmail.com>
2023-01-11 00:40:26 +0200

    remove init and cleanup from logger service

--- src/core/logger/logger.service.js
+++ src/core/logger/logger.service.js
@@ -10,34 +10,6 @@ export default class LoggerService {
      *
      * @type {Logger}
      */
-    static logger = null;
+    static logger = createWinstonLogger();
 
-    /**
-     * Initializes the logger service.
-     *
-     * @return {Promise<void>}
-     */
-    static init = async () => {
-        try {
-            LoggerService.logger = createWinstonLogger();
-            LoggerService.logger.info('[LOGGER] Logger initialized with success!');
-        } catch (error) {
-            console.error('[LOGGER]  Logger failed to be initialized, aborting process!');
-            console.error(error);
-            throw error;
-        }
-    };
-
-    /**
-     * Cleans up anything that "init" may setup.
-     *
-     * @return {Promise<void>}
-     */
-    static cleanup = async () => {
-        if(LoggerService.logger) {
-            LoggerService.logger.close();
-        }
-
-        LoggerService.logger = null;
-    };
 }
