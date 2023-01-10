0a08855d011b44b64dc0cec52dc154148a6aafbf
Borz Alexandru Mihai <alexandru.borz94@gmail.com>
2023-01-10 18:52:48 +0200

    add cleanup to logger service; incorporate logging service status inside its init method

--- src/core/logger/logger.service.js
+++ src/core/logger/logger.service.js
@@ -18,6 +18,26 @@ export default class LoggerService {
      * @return {Promise<void>}
      */
     static init = async () => {
-        LoggerService.logger = createWinstonLogger();
+        try {
+            LoggerService.logger = createWinstonLogger();
+            LoggerService.logger.info('[LOGGER] Logger initialized with success!');
+        } catch (error) {
+            console.error('[LOGGER]  Logger failed to be initialized, aborting process!');
+            console.error(error);
+            throw error;
+        }
+    };
+
+    /**
+     * Cleans up anything that "init" may setup.
+     *
+     * @return {Promise<void>}
+     */
+    static cleanup = async () => {
+        if(LoggerService.logger) {
+            LoggerService.logger.close();
+        }
+
+        LoggerService.logger = null;
     };
 }
